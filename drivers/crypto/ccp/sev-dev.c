8c813d3c394844e6f529c1d965616dafd65f0763
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2022-06-22 11:59:56 +0000

    crypto: ccp: add missing error checks/returns
    When the platform device does not support DMA, the return from probe is
    0 which leads to remove being called later but drvdata has not been set
    which leads to an oops. When the device is being removed and psp_master
    is not set that's another oops unless we check for psp_master.

--- drivers/crypto/ccp/sev-dev.c
+++ drivers/crypto/ccp/sev-dev.c
@@ -782,8 +782,13 @@ EXPORT_SYMBOL_GPL(sev_platform_init);
 
 static int __sev_platform_shutdown_locked(int *error)
 {
-	struct sev_device *sev = psp_master->sev_data;
+	struct sev_device *sev;
 	int ret;
+
+	if (!psp_master)
+		return 0;
+
+	sev = psp_master->sev_data;
 
 	if (sev->state == SEV_STATE_UNINIT)
 		return 0;
@@ -1127,9 +1132,13 @@ EXPORT_SYMBOL_GPL(sev_snp_init);
 
 static int __sev_snp_shutdown_locked(int *error)
 {
-	struct sev_device *sev = psp_master->sev_data;
+	struct sev_device *sev;
 	int ret;
 
+	if (!psp_master)
+		return 0;
+
+	sev = psp_master->sev_data;
 	if (!sev->snp_inited)
 		return 0;
 
