ac7a0fee7b56cc785a53909ad6153013276a8abd
Jeremi Piotrowski <jpiotrowski@linux.microsoft.com>
2023-01-17 10:52:00 +0000

    x86/psp: Register PSP platform device when ASP table is present
    The ASP table contains the memory location of the register window for
    communication with the Platform Security Processor. The device is not exposed
    as an acpi node, so it is necessary to probe for the table and register a
    platform_device to represent it in the kernel.
    
    At least conceptually, the same PSP may be exposed on the PCIe bus as well, in
    which case it would be necessary to choose whether to use a PCI BAR or the
    register window defined in ASPT for communication. There is no advantage to
    using the ACPI and there are no known bare-metal systems that expose the ASP
    table, so device registration is restricted to the only systems known to
    provide an ASPT: Hyper-V VMs. Hyper-V VMs also do not expose the PSP over PCIe.
    
    This is a skeleton device at this point, as the ccp driver is not yet prepared
    to correctly probe it. Interrupt configuration will come later on as well.
    
    Signed-off-by: Jeremi Piotrowski <jpiotrowski@linux.microsoft.com>

---
+++ arch/x86/kernel/psp.c
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <linux/platform_data/psp.h>
+#include <linux/platform_device.h>
+#include <asm/hypervisor.h>
+
+static struct platform_device psp_device = {
+	.name           = "psp",
+	.id             = PLATFORM_DEVID_NONE,
+};
+
+static int __init psp_init_platform_device(void)
+{
+	struct psp_platform_data pdata = {};
+	struct resource res[1];
+	int err;
+
+	/*
+	 * The ACPI PSP interface is mutually exclusive with the PCIe interface,
+	 * but there is no reason to use the ACPI interface over the PCIe one.
+	 * Restrict probing ACPI PSP to platforms known to only expose the ACPI
+	 * interface, which at this time is SNP-host capable Hyper-V VMs.
+	 */
+	if (!hypervisor_is_type(X86_HYPER_MS_HYPERV))
+		return -ENODEV;
+
+	err = acpi_parse_aspt(res, &pdata);
+	if (err)
+		return err;
+	err = platform_device_add_resources(&psp_device, res, 1);
+	if (err)
+		return err;
+
+	err = platform_device_register(&psp_device);
+	if (err)
+		return err;
+	return 0;
+}
+device_initcall(psp_init_platform_device);
