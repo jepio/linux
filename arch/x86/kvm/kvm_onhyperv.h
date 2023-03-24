792baa11623d29cacddebd268911581e788f9874
Jeremi Piotrowski <jpiotrowski@linux.microsoft.com>
2023-03-24 15:52:33 +0100

    KVM: SVM: Flush Hyper-V TLB when required
    The Hyper-V "EnlightenedNptTlb" enlightenment is always enabled when KVM
    is running on top of Hyper-V and Hyper-V exposes support for it (which
    is always). On AMD CPUs this enlightenment results in ASID invalidations
    not flushing TLB entries derived from the NPT. To force the underlying
    (L0) hypervisor to rebuild its shadow page tables, an explicit hypercall
    is needed.
    
    The original KVM implementation of Hyper-V's "EnlightenedNptTlb" on SVM
    only added remote TLB flush hooks. This worked out fine for a while, as
    sufficient remote TLB flushes where being issued in KVM to mask the
    problem. Since v5.17, changes in the TDP code reduced the number of
    flushes and the out-of-sync TLB prevents guests from booting
    successfully.
    
    Split svm_flush_tlb_current() into separate callbacks for the 3 cases
    (guest/all/current), and issue the required Hyper-V hypercall when a
    Hyper-V TLB flush is needed. The most important case where the TLB flush
    was missing is when loading a new PGD, which is followed by what is now
    svm_flush_tlb_current().
    
    Cc: stable@vger.kernel.org # v5.17+
    Fixes: 1e0c7d40758b ("KVM: SVM: hyper-v: Remote TLB flush for SVM")
    Link: https://lore.kernel.org/lkml/43980946-7bbf-dcef-7e40-af904c456250@linux.microsoft.com/
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Jeremi Piotrowski <jpiotrowski@linux.microsoft.com>
    Reviewed-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Message-Id: <20230324145233.4585-1-jpiotrowski@linux.microsoft.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/kvm_onhyperv.h
+++ arch/x86/kvm/kvm_onhyperv.h
@@ -11,6 +11,11 @@ int hv_flush_remote_tlbs_range(struct kvm *kvm, gfn_t gfn, gfn_t nr_pages);
 int hv_flush_remote_tlbs(struct kvm *kvm);
 void hv_track_root_tdp(struct kvm_vcpu *vcpu, hpa_t root_tdp);
 #else /* !CONFIG_HYPERV */
+static inline int hv_flush_remote_tlbs(struct kvm *kvm)
+{
+	return -EOPNOTSUPP;
+}
+
 static inline void hv_track_root_tdp(struct kvm_vcpu *vcpu, hpa_t root_tdp)
 {
 }
