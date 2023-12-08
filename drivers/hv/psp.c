42705b60503dc6217bc64a17a5567a3c7e8793ca
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2023-12-08 16:03:07 +0000

    fixup! Drivers: hv: psp: Add IRQ support

--- drivers/hv/psp.c
+++ drivers/hv/psp.c
@@ -4,6 +4,8 @@
 #include <linux/platform_device.h>
 #include <linux/iopoll.h>
 #include <linux/irq.h>
+#include <linux/acpi.h>
+#include <linux/msi.h>
 #include <asm/apic.h>
 #include <asm/hypervisor.h>
 #include <asm/irqdomain.h>
@@ -40,7 +42,6 @@ struct psp_irq_data {
 	void __iomem *base;
 	u8 mbox_irq_id;
 	int acpi_cmd_resp_reg;
-	struct irq_domain *domain;
 };
 static struct psp_irq_data pspirqd;
 
@@ -79,19 +80,25 @@ static int psp_set_irq_enable(struct psp_irq_data *data, bool irq_en)
 	return 0;
 }
 
-static int psp_configure_irq(struct psp_irq_data *data, unsigned int vector, unsigned int apicid)
+static int psp_configure_irq(struct psp_irq_data *data, const struct msi_msg *msg)
 {
 	void __iomem *reg = data->base + data->acpi_cmd_resp_reg;
+	unsigned int vector, apicid;
 	u16 part1, part2, part3;
+	bool dest_mode_logical;
 	int err;
 
 	if (data->mbox_irq_id > 63)
 		return -EINVAL;
 
+	vector = msg->arch_data.vector;
+	apicid = msg->arch_addr_lo.destid_8_14 << 8 |  msg->arch_addr_lo.destid_0_7;
+	dest_mode_logical = msg->arch_addr_lo.dest_mode_logical;
+
 	part1  = apicid;
 	part2  = apicid >> 16;
 	part3  = vector & PSP_ACPI_VECTOR_MASK;
-	part3 |= apic->dest_mode_logical << PSP_ACPI_DEST_MODE_SHIFT;
+	part3 |= dest_mode_logical << PSP_ACPI_DEST_MODE_SHIFT;
 	part3 |= data->mbox_irq_id << PSP_ACPI_MBOX_IRQID_SHIFT;
 
 	err = psp_sync_cmd(reg, ASP_CMDID_PART1, part1);
@@ -113,24 +120,6 @@ static int psp_configure_irq(struct psp_irq_data *data, unsigned int vector, uns
 	return 0;
 }
 
-static int psp_irq_set_affinity(struct irq_data *data, const struct cpumask *mask, bool force)
-{
-	struct psp_irq_data *pspirqd = irq_data_get_irq_chip_data(data);
-	struct irq_cfg *cfg;
-	int err;
-
-	err = irq_chip_set_affinity_parent(data, mask, force);
-	if (err < 0 || err == IRQ_SET_MASK_OK_DONE)
-		return err;
-
-	cfg = irqd_cfg(data);
-	err = psp_configure_irq(pspirqd, cfg->vector, cfg->dest_apicid);
-	if (err)
-		return err;
-
-	return 0;
-}
-
 static void psp_irq_unmask(struct irq_data *data)
 {
 	struct psp_irq_data *pspirqd = irq_data_get_irq_chip_data(data);
@@ -145,46 +134,33 @@ static void psp_irq_mask(struct irq_data *data)
 	psp_set_irq_enable(pspirqd, false);
 }
 
-static const struct irq_chip psp_irq_chip = {
-	.name			= "PSP-IRQ",
-	.irq_set_affinity	= psp_irq_set_affinity,
-	.irq_ack		= irq_chip_ack_parent,
-	.irq_retrigger  	= irq_chip_retrigger_hierarchy,
-	.irq_mask	     	= psp_irq_mask,
-	.irq_unmask	    	= psp_irq_unmask,
-	.flags			= IRQCHIP_AFFINITY_PRE_STARTUP,
-};
-
-static int psp_irq_domain_alloc(struct irq_domain *domain, unsigned int virq,
-				unsigned int nr_irqs, void *args)
+static void psp_irq_write_msi_msg(struct irq_data *data, struct msi_msg *msg)
 {
-	int err;
-	int i;
-
-	err = irq_domain_alloc_irqs_parent(domain, virq, nr_irqs, args);
-	if (err)
-		return err;
-
-	for (i = virq; i < virq + nr_irqs; i++) {
-		irq_set_chip_and_handler_name(i, &psp_irq_chip, handle_edge_irq, "edge");
-		irq_set_chip_data(i, domain->host_data);
-	}
-	return 0;
+	struct psp_irq_data *pspirqd = irq_data_get_irq_chip_data(data);
+	int err = psp_configure_irq(pspirqd, msg);
+	WARN_ON_ONCE(err);
 }
 
-static const struct irq_domain_ops psp_irq_domain_ops = {
-	.alloc	= psp_irq_domain_alloc,
-	.free	= irq_domain_free_irqs_top,
+static const struct msi_domain_template psp_msi_template = {
+	.chip = {
+		.name = "PSP-MSI",
+		.irq_mask = psp_irq_mask,
+		.irq_unmask = psp_irq_unmask,
+		.irq_write_msi_msg = psp_irq_write_msi_msg,
+		.flags = IRQCHIP_ONESHOT_SAFE,
+	},
+	.info = {
+		.flags = MSI_FLAG_FREE_MSI_DESCS|
+				 MSI_FLAG_DEV_SYSFS|
+				 MSI_FLAG_ACTIVATE_EARLY|
+				 MSI_FLAG_ALLOC_SIMPLE_MSI_DESCS,
+		.bus_token = DOMAIN_BUS_PCI_DEVICE_MSI,
+	}
 };
 
-static int psp_init_irq(const struct psp_platform_data *pdata, const struct resource *reg,
-			struct resource *irq)
+static int psp_init_irq_data(const struct resource *reg, const struct psp_platform_data *pdata)
 {
-	struct irq_alloc_info info;
-	struct fwnode_handle *fn;
 	void __iomem *base;
-	int virq;
-	int err;
 
 	base = ioremap(reg->start, resource_size(reg));
 	if (!base)
@@ -194,42 +170,38 @@ static int psp_init_irq(const struct psp_platform_data *pdata, const struct reso
 	pspirqd.acpi_cmd_resp_reg = pdata->acpi_cmd_resp_reg;
 	pspirqd.base = base;
 
-	fn = irq_domain_alloc_named_fwnode("AMD-PSP-IRQ");
-	if (!fn) {
-		err = -ENOMEM;
-		goto unmap;
-	}
-
-	pspirqd.domain = irq_domain_create_hierarchy(x86_vector_domain, 0, 1,
-							fn,
-							&psp_irq_domain_ops,
-							&pspirqd);
-	if (!pspirqd.domain) {
-		err = -ENOMEM;
-		goto freefwnode;
-	}
-
-	init_irq_alloc_info(&info, NULL);
-	virq = irq_domain_alloc_irqs(pspirqd.domain, 1, NUMA_NO_NODE, &info);
-	if (virq < 0) {
-		err = virq;
-		goto freedomain;
-	}
-	*irq = (struct resource)DEFINE_RES_IRQ(virq);
-
 	return 0;
-
-freedomain:
-	irq_domain_remove(pspirqd.domain);
-	pspirqd.domain = NULL;
-freefwnode:
-	irq_domain_free_fwnode(fn);
-unmap:
-	iounmap(base);
-
-	return err;
 }
 
+int psp_alloc_irq(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	int virq;
+	int err;
+
+	if (!msi_create_device_irq_domain(dev, MSI_DEFAULT_DOMAIN, &psp_msi_template, 1, NULL, &pspirqd)) {
+		dev_err(dev, "failed to ceate MSI domain\n");
+		return -EINVAL;
+	}
+	err = msi_domain_alloc_irqs_range(dev, MSI_DEFAULT_DOMAIN, 0, 0);
+	if (err) {
+		dev_err(dev, "failed to allocate MSI: %d\n", err);
+		return err;
+	}
+	virq = msi_get_virq(dev, 0);
+	if (!virq) {
+		dev_err(dev, "failed to get virq\n");
+		return -ENOENT;
+	}
+	return virq;
+}
+EXPORT_SYMBOL_GPL(psp_alloc_irq);
+
+void psp_free_irq(struct platform_device *pdev)
+{
+	msi_domain_free_irqs_all_locked(&pdev->dev, MSI_DEFAULT_DOMAIN);
+}
+EXPORT_SYMBOL_GPL(psp_free_irq);
 
 static int __init psp_init_platform_device(void)
 {
@@ -250,15 +222,16 @@ static int __init psp_init_platform_device(void)
 	err = acpi_parse_aspt(&res[0], &pdata);
 	if (err)
 		return err;
-	err = psp_init_irq(&pdata, &res[0], &res[1]);
 	psp_device = platform_device_alloc("psp", PLATFORM_DEVID_NONE);
 	if (!psp_device) {
 		pr_err("failed to alloc platform device\n");
 		return -ENOMEM;
 	}
+	dev_set_msi_domain(&psp_device->dev, x86_pci_msi_default_domain);
+	err = psp_init_irq_data(&res[0], &pdata);
 	if (err)
-		return err;
-	err = platform_device_add_resources(&psp_device, res, 2);
+		goto err;
+	err = platform_device_add_resources(psp_device, res, 1);
 	if (err)
 		goto err;
 	err = platform_device_add_data(psp_device, &pdata, sizeof(pdata));
